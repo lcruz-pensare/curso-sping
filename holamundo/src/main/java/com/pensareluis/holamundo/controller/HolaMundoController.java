414370ac33bb456daf111db39d0e800e5d919976
lcruz-pensare <jreyes@pensare.mx>
2018-03-21 11:06:56 -0600

    Proyecto de spring funcionando

---
+++ holamundo/src/main/java/com/pensareluis/holamundo/controller/HolaMundoController.java
@@ -0,0 +1,14 @@
+package com.pensareluis.holamundo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Jorge Álvarez
+ * @version 1.0 - 2018/03/21
+ */
+public class HolaMundoController {
+
+
+
+}
