8426568f75e90d8675a8bcfb37db73a474c45983
Luis Cruz <lcruz@pensare.mx>
2018-03-22 12:38:50 -0600

    Ejemplos de get

--- holamundo/src/main/java/com/pensareluis/holamundo/controller/ExampleController.java
+++ holamundo/src/main/java/com/pensareluis/holamundo/controller/ExampleController.java
@@ -1,10 +1,15 @@
 package com.pensareluis.holamundo.controller;
 
+import com.pensareluis.holamundo.controller.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/example")
@@ -12,13 +17,25 @@ public class ExampleController {
     public static final String EXAMPLE_VIEW= "example";
     //Primera forma
     @GetMapping("exampleString")
-    public String exampleString()
+    public String exampleString(Model model)
     {
+        model.addAttribute("people",getPeople());
         return EXAMPLE_VIEW;
     }
     //Segunda forma
      @GetMapping("exampleMAV")
     public ModelAndView exampleMAV(){
-        return new ModelAndView(EXAMPLE_VIEW);//Comentario Diferenciador
+        ModelAndView mov= new ModelAndView(EXAMPLE_VIEW);
+        mov.addObject("people",getPeople());
+        return  mov;
+    }
+
+    private List<Person> getPeople(){
+        List<Person> people = new ArrayList<>();
+        people.add(new Person ("Jhon", 23));
+        people.add(new Person ("Luis", 25));
+        people.add(new Person ("Rafael", 30));
+        people.add(new Person ("Naye", 27));
+        return people;
     }
 }
